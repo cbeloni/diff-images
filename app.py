e50688ebe9fb06d76395c55bb4c4ec7714f150a1
Caue <cbeloni@gmail.com>
2021-04-18 10:21:52 -0300

    Alterado porta

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def listar_todos():
     return json_text
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
